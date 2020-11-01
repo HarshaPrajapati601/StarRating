9633fbf21d26b0fa498b834019196c3c9865b616
HarshaPrajapati <harsha_1605@yahoo.com>
2020-11-01 15:18:12 +0530

    star rating updation

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,19 +4,39 @@ import Star from './Star';
 class StarRating extends Component {
 
   // Initialize a 'rating' state
-state ={
-  rating:0
-}
-starsArray=[];
+  state ={
+    rating:0
+  }
+
 
   // Write a function that returns 5 Star components
- StarRender = ()=>{
-  for(let i=0; i<=5;i++){
-    this.starsArray.push(i);
+  StarRender = ()=>{
+    let starsArray=[];
+    for(let i=0; i<5;i++){
+      starsArray.push(
+        <Star key={i}
+         starUpdate={()=>this.handleSetRating(i+1)}
+         isSelected={this.state.rating > i}
+        />
+      );
+    }
+    return starsArray;
   }
- }
 
   // Write an event handler that updates the rating state.
+  handleSetRating =(rating)=>{
+    if(this.state.rating === rating) //if 4selected === 4 desected
+      {
+        this.setState({
+          rating:0
+        })
+      }else{
+        this.setState({
+          rating:rating
+        })
+      }
+ 
+  }
   // Pass the function to a Star component via props. ///****///
 
 
@@ -24,7 +44,7 @@ starsArray=[];
     return (
       <ul className="course--stars">
         {/* Render the Star components */}
-        <Star  />
+        {this.StarRender()}
       </ul>
     );
   }
